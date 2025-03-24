5ba1a1e3c8cae9102f4ca11fc375dfa70df3b246
Aslam Abdika <aslamabdika@gmail.com>
2025-03-24 23:32:00 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -11,5 +12,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/rpafrontend/', // Ganti dengan nama repo GitHub Anda
 })
